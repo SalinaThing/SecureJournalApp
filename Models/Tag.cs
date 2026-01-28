69cffbed92739140e389be06003e8c6e592c935d
Salina <salinathing667@gmail.com>
2026-01-28 11:14:35 +0545

    Removed Tag.cs

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SQLite;
 
-namespace SecureJournalApp.Models
-{
-    public class Tag
-    {
-        [PrimaryKey, AutoIncrement]
-        public int TagId { get; set; }
-
-    }
-}
