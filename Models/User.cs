8cc053464e981d26e6e05949621cc9d332ffdcff
Salina <salinathing667@gmail.com>
2026-01-28 11:13:56 +0545

    Removed User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,14 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SecureJournalApp.Models
-{
-    public class User
-    {
-        public int Id { get; set; }
-        public string Pin { get; set; } = string.Empty; // store hashed PIN ideally
-    }
-}
